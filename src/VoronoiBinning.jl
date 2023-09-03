c1750df2a0b56c271f97daeb12c2f8e6a9e004a1
Michael-Reefe <michael.reefe8@gmail.com>
2023-09-02 22:19:38 -0400

    updated version

--- src/VoronoiBinning.jl
+++ src/VoronoiBinning.jl
@@ -16,9 +16,11 @@ using LoopVectorization
 
 const DO_PLOTTING = try
     using PyPlot
+    plt.plot()
+    plt.close()
     true
 catch
-    println("WARNING: PyPlot package not found. The plotting capability will be disabled!")
+    println("WARNING: PyPlot or matplotlib not found. The plotting capability will be disabled!")
     false
 end
 
